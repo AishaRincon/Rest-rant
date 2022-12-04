4e31e9054f4cf4b75a6b2d5f2a8268c97e0aa7c8
AishaRincon <100747969+AishaRincon@users.noreply.github.com>
2022-12-04 14:44:41 -0800

    added Bootstrap grid and images to index page

--- views/home.jsx
+++ views/home.jsx
@@ -6,15 +6,15 @@ function home () {
       <Default>
           <main>
               <h1>HOME</h1>
+              <a href="/places">
+                <button className="btn-primary">Places Page</button>
+              </a>
               <div>
                 <img src="/images/rest-rant-img.jpg" alt="Dining out" />
               </div>
                 <div>
                   Photo by<a href="AUTHOR_LINK">Jay Wennington</a> on <a href="https://unsplash.com/s/photos/food">Unsplash</a>
                 </div>
-                <a href="/places">
-                <button className="btn-primary">Places Page</button>
-                </a>
             </main>
       </Default>
     )
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,17 +4,25 @@ const Default = require('../default');
 function index (data) {
     let placesFormatted = data.places.map((place) =>{
     return (
-      <div>
+      <div className="col-sm-6">
         <h2>{place.name}</h2>
+        <p className="text-center">
+          {place.cuisines}
+        </p>
         <img src={place.pic} alt={place.name}/>
+        <p className="text-center">
+          Located in {place.city}, {place.state}
+        </p>
       </div>
     )
   })  
     return (
         <Default>
         <main>
-            <h1>PLACES INDEX PAGE</h1>
+            <h1>Restuarant to Rant or Rave about</h1>
+            <div className="row">
             {placesFormatted}
+            </div>
         </main>
         </Default>
     )
