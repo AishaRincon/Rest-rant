982d68613d2e11b37e7203bdd5ce25549a776380
AishaRincon <100747969+AishaRincon@users.noreply.github.com>
2022-12-04 14:12:05 -0800

    added image to 404 page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404() {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+              <img src="/images/404Flamingo.jpg" alt="Flamingo Floatie Error Image" />
+              </div>
+              <div>
+                  Photo by<a href="AUTHOR_LINK">Vicko Mozara</a> on <a href="https://unsplash.com/s/photos/beach">Unsplash</a>
+                </div>
           </main>
       </Default>
     )
