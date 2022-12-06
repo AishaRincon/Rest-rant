ca7aa1a290d223d9b0f8542fa051a07ec7c0b2b0
AishaRincon <100747969+AishaRincon@users.noreply.github.com>
2022-12-05 16:12:32 -0800

    added form to new page

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,6 +6,29 @@ function new_form () {
         <Default>
             <main>
                 <h1> Add a new place</h1>
+                <form method='POST' action="/places">
+                    <div>
+                        <label htmlfor="name"> Place Name</label>
+                        <input id="name" name="name" required/>
+                    </div>
+                    <div>
+                        <label htmlFor="pic">Place Picture</label>
+                        <input type="url" id="pic" name="pic" />
+                    </div>
+                    <div>
+                        <label htmlfor="city"> Place City</label>
+                        <input id="city" name="city" />
+                    </div>
+                    <div>
+                        <label htmlfor="state"> Place State</label>
+                        <input id="state" name="state" />
+                    </div>
+                    <div>
+                        <label htmlfor="cuisiness"> Place Name</label>
+                        <input id="cusiness" name="cusiness" required/>
+                    </div>
+                    <input type ="submit" value="Add Place" />
+                </form>
             </main>
         </Default>
     )
